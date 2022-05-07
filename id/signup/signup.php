afb11acdc5706b89fc9cc09ef959204ecdb4c568
luminosity <agerio100@naver.com>
2022-05-07 16:23:23 +0900

    회원가입 양식 front단 기본 만들기
    회원가입 양식을 그럭저럭 만들었다. backend 단에서의 동작이 필요하다.

---
+++ id/signup/signup.php
@@ -0,0 +1,91 @@
+<?php
+    header('Content-Type: text/html; charset=utf-8');
+?>
+
+<html>
+
+    <head>
+
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>루메스 보드 회원가입</title>
+        <link rel="stylesheet" href="../../css/font.css">
+        <link rel="stylesheet" href="./css/signup.css">
+        <link rel="shortcut icon" href="../../favicon/favicon.ico">
+
+        <?php include('../../common/resource.html'); ?>
+
+    </head>
+
+    <body>
+
+        <!-- 간단한 상단 바 -->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+
+                <a class="navbar-brand" href="./index.php">📟 board.lumes.kr</a>
+
+                <!-- 메뉴 고르기 -->
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="http://lumes.kr">LUMES.KR로 돌아가기</a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- 메뉴 고르기 끝 -->
+
+            </div>
+        </nav>
+        <!-- 간단한 상단 바 끝 -->
+
+        <!-- 회원가입 폼 -->
+        <form class="registration-form" action="signupProcess.php" method="POST">
+
+            <h3 id="welcomeText">WELCOME!</h3>
+
+            <div class="form-floating">
+                <input type="text" class="form-control" id="userID" placeholder="ID" aria-describedby="userIDHelp">
+                <label for="userID">ID</label>
+                <div id="userIDHelp" class="form-text">~30바이트, ID로는 영어대소문자/숫자/언더바(_)만 사용하실 수 있습니다.</div>
+            </div>
+
+            <div class="form-floating">
+                <input type="text" class="form-control" id="userNickname" placeholder="유저 닉네임" aria-describedby="userNicknameHelp">
+                <label for="userNickname">유저 닉네임</label>
+                <div id="userNicknameHelp" class="form-text">~255바이트, 여기서 사용할 닉네임을 만드세요.</div>
+            </div>
+
+            <div class="row">
+                <div class="col form-floating">
+                    <input type="password" class="form-control" id="userPassword1" placeholder="&nbsp;&nbsp;비밀번호">
+                    <label for="userPassword1">&nbsp;&nbsp;비밀번호</label>
+                </div>
+
+                <div class="col form-floating">
+                    <input type="password" class="form-control" id="userPassword2" placeholder="&nbsp;&nbsp;비밀번호(재입력)">
+                    <label for="userPassword2">&nbsp;&nbsp;비밀번호(재입력)</label>
+                </div>
+            </div>
+
+            <div class="form-floating">
+                <input type="email" class="form-control" id="userEmail" placeholder="유저 이메일" aria-describedby="userEmailHelp">
+                <label for="userEmail">이메일 주소</label>
+                <div id="userEmailHelp" class="form-text">나중에 인증 등에 필요할 수 있으니 사용하시는 이메일 주소를 입력하세요.</div>
+            </div>
+
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                <label class="form-check-label" for="flexCheckDefault">
+                    나는 취약점 제보 등의 내용을 포함한 모든 사용약관을 숙지하고 동의합니다.
+                </label>
+            </div>
+
+            <button type="submit" id="registrationSubmitButton" class="btn btn-outline-primary">가입하기</button>
+
+        </form>
+        <!-- 회원가입 폼 끝 -->
+
+    </body>
+
+</html>
--- index.php
+++ index.php
@@ -5,28 +5,26 @@
 
 <!DOCTYPE html>
 
-<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>루메스 보드</title>
+        <link rel="stylesheet" href="./css/index.css">
+        <link rel="stylesheet" href="./css/navbar.css">
+        <link rel="stylesheet" href="./css/font.css">
+        <link rel="shortcut icon" href="./favicon/favicon.ico">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>루메스 보드</title>
-    <link rel="stylesheet" href="./css/index.css">
-    <link rel="stylesheet" href="./css/navbar.css">
-    <link rel="stylesheet" href="./css/font.css">
-    <link rel="shortcut icon" href="./favicon/favicon.ico">
-
-    <!-- CDN 관련 자료들을 한번에 관리 -->
-    <?php include('./common/resource.html') ?>
+        <!-- CDN 관련 자료들을 한번에 관리 -->
+        <?php include('./common/resource.html') ?>
 
 
-</head>
+    </head>
 
-<body>
+    <body>
 
-    <!-- 네비게이션 바 -->
-    <?php include("navbar.php"); ?>
+        <!-- 네비게이션 바 -->
+        <?php include("navbar.php"); ?>
 
-</body>
+    </body>
 
 </html>
