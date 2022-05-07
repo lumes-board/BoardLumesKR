a9ce37cf62c0597d92b2463027af0eaef069d6be
luminosity <agerio100@naver.com>
2022-05-07 15:05:25 +0900

    기본적인 네비게이션 바 만들기
    로그인 여부와 상관없이, 제일 기본이 될 네비게이션 바를 만들었다.

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>루메스 보드</title>
     <link rel="stylesheet" href="./css/index.css">
+    <link rel="stylesheet" href="./css/navbar.css">
     <link rel="stylesheet" href="./css/font.css">
     <link rel="shortcut icon" href="./favicon/favicon.ico">
 
--- navbar.php
+++ navbar.php
@@ -1,5 +1,46 @@
-<nav class="navbar navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="./index.php">🎫 Lumes Board</a>
+
+        <a class="navbar-brand" href="./index.php">📟 board.lumes.kr</a>
+
+        <!-- 메뉴 고르기 -->
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="http://lumes.kr">LUMES.KR로 돌아가기</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        더 알아보기
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" target="_blank"
+                                href="https://github.com/lumes-board/BoardLumesKR">📜 Github</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                            <li><a class="dropdown-item" target="_blank" href="https://blog.naver.com/agerio100">📘 개발자 블로그</a>
+                        </li>
+                            <li><a class="dropdown-item" target="_blank" href="https://twitter.com/LUM1N1OUS">📢 개발자 트위터</a>
+                        </li>
+                        </li>
+                            <li><a class="dropdown-item" target="_blank" href="#">✨ Hall of Fame</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="http://cvs.lumes.kr" tabindex="-1" aria-disabled="true">(누를 수 없는
+                        버튼!)</a>
+                </li>
+            </ul>
+        </div>
+        <!-- 메뉴 고르기 끝 -->
+
+        <!-- 버튼 -->
+        <button class="btn btn-success navbar-button" type="button">회원가입</button>
+        <button class="btn btn-primary navbar-button" type="button">로그인</button>
+        <!-- 버튼 끝 -->
+
     </div>
 </nav>
