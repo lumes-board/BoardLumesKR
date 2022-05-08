4b073f639abfbc78a8a313ff82680a88e826bbae
luminosity <agerio100@naver.com>
2022-05-08 20:42:28 +0900

    마이페이지 기능 추가
    유저의 현재 정보에 대한 간략한 내용을 담은 표를 마이페이지에 추가했다.

--- id/mypage/mypage.php
+++ id/mypage/mypage.php
@@ -24,6 +24,50 @@
         
         <?php include('../../navbar.php'); ?>
 
+        <?php 
+
+            // 유저 정보 테이블에 사용될 정보를 가져오기
+            $query = "SELECT * FROM member WHERE id = :id";
+            $stmt = $db->prepare($query);
+            $stmt->bindParam(':id', $_SESSION['id'], PDO::PARAM_STR); 
+            
+            $stmt->execute();
+
+            $userInfoData = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            $email                  = $userInfoData[0]["email"];
+            $nickname               = $userInfoData[0]["nickname"];
+            $exp                    = $userInfoData[0]["exp"];
+            $expTransactionQty      = $userInfoData[0]["expTransactionQty"];
+            $guestbookQty           = $userInfoData[0]["guestbookQty"];
+
+        ?>
+
+        <!-- 유저 정보 테이블 -->
+        <table class="table text-center" id="userInfoTable">
+            <thead>
+                <tr class="table-dark">
+                    <th>ID</th>
+                    <th>이메일</th>
+                    <th>닉네임</th>
+                    <th>보유경험치</th>
+                    <th>경험치송금량</th>
+                    <th>작성한 게시글</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td><?php echo $_SESSION['id'] ?></td>
+                    <td><?php echo $email ?></td>
+                    <td><?php echo $nickname ?></td>
+                    <td><?php echo number_format($exp) ?> <span style="color: gray; ">EXP</span></td>
+                    <td><?php echo number_format($expTransactionQty) ?> <span style="color: gray; ">EXP</span></td>
+                    <td><?php echo number_format($guestbookQty) ?> <span style="color: gray; ">개</span></td>
+                </tr>
+            </tbody>
+        </table>
+        <!-- 유저 정보 테이블 끝 -->
+
     </body>
 
 </html>
