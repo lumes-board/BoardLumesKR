785eba7ade736662cd36910c20467c4aeb31364d
luminosity <agerio100@naver.com>
2022-06-01 21:23:50 +0900

    ticker 애니메이션 JS값 수정
    애니메이션이 자연스럽고 읽기 좋게 수정되었음.

--- board/js/tickerNotification.js
+++ board/js/tickerNotification.js
@@ -2,7 +2,7 @@ let rolling = document.getElementById("tickerNotificationRoller");
  
 window.setInterval( function () {
  
-	rolling.style.transitionDuration = "1200ms";
+	rolling.style.transitionDuration = "400ms";
 	rolling.style.marginTop = "-2em";
  
 	window.setTimeout( function () {
@@ -10,5 +10,5 @@ window.setInterval( function () {
 		rolling.removeAttribute("style");         
 		rolling.appendChild(rolling.firstElementChild);
         
-	}, 400);
-}, 2000);
+	}, 2400);
+}, 2800);
